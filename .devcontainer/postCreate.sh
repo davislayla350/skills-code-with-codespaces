8f04d0e4803ccb36d64bc022e6e8fb295cb45532
LayLa Davis <davis.layla350@gmail.com>
2026-07-15 00:38:52 +0000

    feat: Add features, extensions, and postCreate script

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"postCreateCommand": ".devcontainer/postCreate.sh"
+sudo apt-get update
+sudo apt-get install sl
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
